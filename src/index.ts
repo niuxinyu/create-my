0e04f30fe21f5a363aefe7279c32eb486013e502
niuxinyu <2659546337@qq.com>
2023-09-19 14:51:49 +0800

    feat:优化;更新依赖;移除prettier;

--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,79 @@
-import { execSync } from 'node:child_process';
-import { copyFileSync, mkdirSync, readFileSync, statSync, writeFileSync } from 'node:fs';
-import { join, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
+import { execSync } from 'node:child_process'
+import {
+  copyFileSync,
+  mkdirSync,
+  readFileSync,
+  statSync,
+  writeFileSync,
+} from 'node:fs'
+import { join, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
-import detectIndent from 'detect-indent';
-import { detectNewline } from 'detect-newline';
+import detectIndent from 'detect-indent'
+import { detectNewline } from 'detect-newline'
 
 const pkgPath = resolve(
   fileURLToPath(import.meta.url),
   '../..',
   'package.json',
-);
+)
 
-const root = process.cwd();
+const root = process.cwd()
 
 function readFileString(path: string) {
   return {
     data: readFileSync(path).toString(),
     path,
-  };
+  }
 }
 
 function readFileJSON(path: string) {
-  const { data } = readFileString(path);
-  const newLine = detectNewline(data);
-  const indent = detectIndent(data).indent;
+  const { data } = readFileString(path)
+  const newLine = detectNewline(data)
+  const indent = detectIndent(data).indent
   return {
     data: JSON.parse(data),
     newLine,
     indent,
-  };
+  }
 }
 
-function log(...args: any) {
-  // eslint-disable-next-line no-console
-  console.log(...args);
+function log(...args: unknown[]) {
+  globalThis.console.log(...args)
 }
 
-const version = process.argv.slice(2);
-const versions = ['--version', '-v'];
+const version = process.argv.slice(2)
+const versions = ['--version', '-v']
 if (Array.isArray(version) && versions.includes(version[0])) {
-  const pkg = readFileJSON(pkgPath);
+  const pkg = readFileJSON(pkgPath)
   if (pkg && pkg.data) {
-    log(pkg.data.version);
-    process.exit(0);
+    log(pkg.data.version)
+    process.exit(0)
   }
 }
 
 function isFileExit(path: string) {
   try {
-    return readFileString(path);
+    return !!readFileString(path)
   } catch (e) {
-    return false;
+    return false
   }
 }
 
 function isDirExit(path: string) {
   try {
-    const stat = statSync(path);
+    const stat = statSync(path)
     if (stat && stat.isDirectory()) {
-      return true;
+      return true
     }
-    return true;
+    return true
   } catch (e) {
-    return false;
+    return false
   }
 }
 
 function getRootPath(name: string) {
-  return resolve(root, name);
+  return resolve(root, name)
 }
 
 function getTemplatePath() {
@@ -76,16 +81,16 @@ function getTemplatePath() {
     fileURLToPath(import.meta.url),
     '../..',
     'template-my',
-  );
+  )
 }
 
 function addToGit() {
   if (isDirExit(join(root, '.git'))) {
-    execSync('git add .');
+    execSync('git add .')
   } else {
-    execSync('git init && git add .');
+    execSync('git init && git add .')
   }
-  log('🚚\u0020\u0020Git 完成...');
+  log('🚚 \u0020\u0020Git init success...')
 }
 
 // 先考虑这几种
@@ -95,7 +100,7 @@ const eslintrcs = [
   '.eslintrc.js',
   '.eslintrc.mjs',
   '.eslintrc.cjs',
-];
+]
 
 const prettierrcs = [
   '.prettierrc',
@@ -103,11 +108,11 @@ const prettierrcs = [
   '.prettierrc.js',
   '.prettierrc.cjs',
   '.prettierrc.mjs',
-];
+]
 
 const editorconfigs = [
   '.editorconfig',
-];
+]
 
 const packages = [
   {
@@ -115,135 +120,135 @@ const packages = [
     version: '^8.36.0',
   },
   {
-    name: 'prettier',
-    version: '^2.8.4',
-  },
-  {
     name: '@koalan/eslint-config',
-    version: '*',
+    version: 'latest',
   },
-  {
-    name: '@koalan/prettier-config',
-    version: '*',
-  },
-];
+]
 
 const vscodes = [
   'settings.json',
-  // 'extensions.json',
-];
+]
 
 function addESlint() {
   if (!eslintrcs.some((c) => isFileExit(c))) {
-    const eslintrc = readFileString(join(getTemplatePath(), eslintrcs[0]));
+    const eslintrc = readFileString(join(getTemplatePath(), eslintrcs[0]))
     if (eslintrc) {
       writeFileSync(
         join(root, eslintrcs[0]),
         eslintrc.data,
-      );
-      log('🚚\u0020\u0020ESLint 完成...');
+      )
+      log('🚚 eslint init success ...')
     }
   } else {
-    log('🛠️\u0020\u0020ESLint 跳过...');
+    log('🛠️ already have eslint config ...')
   }
 }
 
 function addPrettier() {
   if (!prettierrcs.some((c) => isFileExit(c))) {
-    const prettierrc = readFileString(join(getTemplatePath(), prettierrcs[0]));
+    const prettierrc = readFileString(join(getTemplatePath(), prettierrcs[0]))
     if (prettierrc) {
       writeFileSync(
         join(root, prettierrcs[0]),
         prettierrc.data,
-      );
-      log('🚚\u0020\u0020Prettier 完成...');
+      )
+      log('🚚 prettier init success ...')
     }
   } else {
-    log('🛠️\u0020\u0020Prettier 跳过...');
+    log('🛠️ already have prettier config ...')
   }
 }
 
 function addEditorConfig() {
   if (!editorconfigs.some((c) => isFileExit(c))) {
-    const editorconfig = readFileString(join(getTemplatePath(), editorconfigs[0]));
+    const editorconfig = readFileString(
+      join(getTemplatePath(),
+        editorconfigs[0]),
+    )
+
     if (editorconfig) {
       writeFileSync(
         join(root, editorconfigs[0]),
         editorconfig.data,
-      );
-      log('🚚\u0020\u0020EditorConfig 完成...');
+      )
+      log('🚚 editorconfig init success ...')
     }
   } else {
-    log('🛠️\u0020\u0020EditorConfig 跳过...');
+    log('🛠️ already have editorconfig ...')
   }
 }
 
 function addPackageJSON() {
-  const { data, newLine, indent } = readFileJSON(join(root, './package.json'));
-  const devPkg = data.devDependencies ?? (data.devDependencies = {});
+  const packageJsonRoot = join(root, './package.json')
+  if (!isFileExit(packageJsonRoot)) {
+    log('No package.json found... ')
+    throw new Error('No package.json found... ')
+  }
+  const { data, newLine, indent } = readFileJSON(packageJsonRoot)
+  const devPkg = data.devDependencies ?? (data.devDependencies = {})
   packages.forEach((p) => {
-    devPkg[p.name] = devPkg[p.name] ?? p.version;
-  });
+    devPkg[p.name] = devPkg[p.name] ?? p.version
+  })
 
-  let json = JSON.stringify(data, undefined, indent);
+  let json = JSON.stringify(data, undefined, indent)
   if (newLine) {
-    json += newLine;
+    json += newLine
   }
 
   writeFileSync(
     join(root, './package.json'),
     json,
-  );
+  )
 }
 
 function addGitIgnore() {
-  const src = join(getTemplatePath(), './.gitignore');
-  const path = join(root, './.gitignore');
+  const src = join(getTemplatePath(), './.gitignore')
+  const path = join(root, './.gitignore')
   if (!isFileExit(path)) {
-    const gitignore = readFileString(src);
+    const gitignore = readFileString(src)
     if (gitignore.data) {
       writeFileSync(
         path,
         gitignore.data,
-      );
-      log('🚚\u0020\u0020Gitignore 完成...');
+      )
+      log('🚚 .gitignore init success ...')
     }
   } else {
-    log('🛠️\u0020\u0020Gitignore 跳过...');
+    log('🛠️ already have .gitignore...')
   }
 }
 
 function addVsCode() {
-  const dir = '.vscode';
-  const path = join(root, `./${dir}`);
-  const src = join(getTemplatePath(), `./${dir}`);
+  const dir = '.vscode'
+  const path = join(root, `./${dir}`)
+  const src = join(getTemplatePath(), `./${dir}`)
   if (!isDirExit(getRootPath('./.vscode'))) {
-    mkdirSync(path, undefined);
+    mkdirSync(path, undefined)
     vscodes.forEach((c) => {
       copyFileSync(
         join(src, `./${c}`),
         join(path, `./${c}`),
-      );
-    });
-    log('🚚\u0020\u0020.vscode 完成...');
+      )
+    })
+    log('🚚 .vscode init success ...')
   } else {
-    log('🛠️\u0020\u0020.vscode 跳过...');
+    log('🛠️ already have .vscode ...')
   }
 }
 
 function init() {
-  addESlint();
-  addPrettier();
-  addEditorConfig();
-  addPackageJSON();
-  addGitIgnore();
-  addVsCode();
-  addToGit();
-  log('🥳\u0020\u0020...All Done!!');
+  addPackageJSON()
+  addESlint()
+  // addPrettier()
+  addEditorConfig()
+  addGitIgnore()
+  addVsCode()
+  addToGit()
+  log('🥳 ... All Done!!!')
 }
 
 try {
-  init();
+  init()
 } catch (e) {
-  console.error(e);
+  console.error(e)
 }
